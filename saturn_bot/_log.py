bbe94be4ae21c7f69bf50ef6b47dcf13927e9b5f
Markus Meyer <hydrantanderwand@gmail.com>
2025-07-02 19:16:19 +0200

    fix lint issues

--- saturn_bot/_log.py
+++ saturn_bot/_log.py
@@ -1,3 +1,7 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
 import logging
 
 
@@ -16,6 +20,7 @@ class _Formatter(logging.Formatter):
 
     def usesTime(self):
         return False
+
 
 def _configure_logging() -> None:
     rl = logging.getLogger()
